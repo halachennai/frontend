2699056d378860f40e73634c1a78a20c5fdcdb98
CowTheGreat <cowara987@gmail.com>
2024-10-20 22:43:14 +0530

    added rs to items

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -16,8 +16,8 @@ const item = (props) => {
 
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old_price}</div>
+        <div className="item-price-new">Rs.{props.new_price}</div>
+        <div className="item-price-old">Rs.{props.old_price}</div>
       </div>
     </div>
   );
